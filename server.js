c4408ccd6bc0e67e6e06bb171ec8d13b2cf205a4
Draraedus <adriel2002santos@gmail.com>
2023-07-27 23:06:13 -0300

    fix in server

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ const port = 3001
 app.use(express.json())
 
 app.use((req, res, next) => {
-  // res.setHeader('Access-Control-Allow-Origin', 'https://portalweb-srag.vercel.app')
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Origin', 'https://portalweb-srag.vercel.app')
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   next()
